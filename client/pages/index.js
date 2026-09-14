52168d915f5a1efc08179abe27a261c87927e311
Dmitry Lenchevsky <49116427+DimitryLenchevsky@users.noreply.github.com>
2026-09-14 12:35:38 +0200

    sd

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,32 @@
-export default () => {
+import axios from "axios";
+import http from "http";
+
+const LandingPage = ({ currentUser }) => {
+  console.log("Current User: ", currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  try {
+    if (typeof window === "undefined") {
+      const { data } = await axios.get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        // "http://auth-srv:3000/api/users/currentuser",
+        {
+          headers: req.headers,
+          httpAgent: new http.Agent({ keepAlive: false }),
+          timeout: 5000,
+        },
+      );
+      return data;
+    } else {
+      const { data } = await axios.get("/api/users/currentuser");
+      return data;
+    }
+  } catch (err) {
+    console.log("ERROR in getInitialProps:", err.message, err.response?.status);
+    return {};
+  }
+};
+
+export default LandingPage;
